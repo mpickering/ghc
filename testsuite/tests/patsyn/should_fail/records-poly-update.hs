80e7e13b8572ba99064da6c28615cf8dfcbc3297
Matthew Pickering <matthewtpickering@gmail.com>
2015-09-12 15:51:43 +0200

    Update tests

--- testsuite/tests/patsyn/should_fail/records-poly-update.hs
+++ testsuite/tests/patsyn/should_fail/records-poly-update.hs
@@ -4,10 +4,12 @@ module Main where
 pattern ReqNoProv :: () => Show a => a -> Maybe a
 pattern ReqNoProv{j} = Just j
 
-p1 = ReqNoProv True
+data A = A deriving Show
 
-p7 (ReqNoProv _) = ReqNoProv False
+p1 = Just True
 
-p6 = p1 {j = False}
+p6 = p1 {j = A}
 
 main = print p6
+
+
