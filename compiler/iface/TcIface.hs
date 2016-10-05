ef7fc1a15bc64084589d3b63789f2d03f5bf6cf0
Joachim Breitner <mail@joachim-breitner.de>
2016-10-05 17:29:53 -0400

    Deserialize interface tuples to ConApp

--- compiler/iface/TcIface.hs
+++ compiler/iface/TcIface.hs
@@ -1088,8 +1088,8 @@ tcIfaceExpr (IfaceTuple sort args)
                UnboxedTuple -> map (Type . getRuntimeRep "tcIfaceExpr") con_tys ++ some_con_args
                _            -> some_con_args
                         -- Put the missing type arguments back in
-             con_id   = dataConWorkId (tyConSingleDataCon tc)
-       ; return (mkApps (Var con_id) con_args) }
+             dc = tyConSingleDataCon tc
+       ; return (mkConApp dc con_args) }
   where
     arity = length args
 
