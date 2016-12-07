fbe5308008cca9b8684ca82784938e22b1951b3f
Matthew Pickering <matthewtpickering@gmail.com>
2016-12-07 14:27:01 +0000

    Two more

---
+++ testsuite/tests/pmcheck/complete_sigs/completesig07.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -Wall #-}
+module Completesig07 where
+
+-- Some overlapping examples
+
+data T = A | B | C
+data S = D | E | F
+
+{-# COMPLETE A, B #-}
+{-# COMPLETE D #-}
+
+m1 :: T -> ()
+m1 A = ()
+m1 A = ()
+m1 B = ()
+
+m2 :: T -> S -> ()
+m2 A D = ()
+m2 B D = ()
+m2 A D = ()
