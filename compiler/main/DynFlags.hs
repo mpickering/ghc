5b32971170cb273f91f9e01278109bd3e1430cb3
Matthew Pickering <matthewtpickering@gmail.com>
2017-01-31 22:07:35 +0000

    Fix typo

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -3662,7 +3662,7 @@ fFlagsDeps = [
   flagSpec "vectorise"                        Opt_Vectorise,
   flagSpec "version-macros"                   Opt_VersionMacros,
   flagSpec "worker-wrapper"                   Opt_WorkerWrapper,
-  flagSpac "solve-constant-dicts"             Opt_SolveConstantDicts,
+  flagSpec "solve-constant-dicts"             Opt_SolveConstantDicts,
   flagSpec "show-warning-groups"              Opt_ShowWarnGroups,
   flagSpec "hide-source-paths"                Opt_HideSourcePaths,
   flagSpec "show-hole-constraints"            Opt_ShowHoleConstraints
